e219ef2c2be6b81485841e20845e9f319fd17650
budimanlai <budiman.lai@gmail.com>
2022-12-21 10:02:16 +0700

    fix scripts/compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,8 +2,8 @@
 
 GIT=`git log | head -n 1 | cut  -f 2 -d ' ' | head -c 8`
 
-MAIN="cmd/app/*.go"
-APP="midtrans_callback"
+MAIN="*.go"
+APP="midtrans_charge"
 FILE="dist/$APP-$GIT"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ${FILE} $MAIN
